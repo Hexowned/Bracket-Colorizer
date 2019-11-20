0c0d1015b1e9fd0ca3be2df1141ebb5353b9ec40
Nexowned <49051663+Nexowned@users.noreply.github.com>
2019-11-19 16:36:44 -0800

    Starting project over as I was running into issues :)

--- BracketPairColorizer/Properties/AssemblyInfo.cs
+++ BracketPairColorizer/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("BracketColorizer")]
+[assembly: AssemblyTitle("BracketPairColorizer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BracketColorizer")]
+[assembly: AssemblyProduct("BracketPairColorizer")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -19,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("df23524a-dfdd-4ec3-9d05-7fdf07b55363")]
+[assembly: Guid("db007425-4912-45cb-9673-e7859475da71")]
 
 // Version information for an assembly consists of the following four values:
 //
