98b13164ac98205647ab41b0b9a643a9d943889d
Nexowned <49051663+Nexowned@users.noreply.github.com>
2019-11-19 16:54:10 -0800

    Initialized setup structure for the project Added global properties Added custom assembly info Added .editorconfig rules

---
+++ AssemblyInfo.Common.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Nexowned")]
+[assembly: AssemblyProduct("BracketPairColorizer")]
+[assembly: AssemblyCopyright("Copyright © Ian Church")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion(BpcVersion.Version)]
+
+internal static class BpcVersion
+{
+    public const String Version = "1.0.0.0";
+}
