4b375da3d8a0b488b0102a05230802642b66aac7
Nexowned <49051663+Nexowned@users.noreply.github.com>
2019-11-22 15:43:25 -0800

    Added XLang support (FakeXLang)

---
+++ BracketPairColorizer.Core/XLangSupport/FakeContentTypeDefinition.cs
@@ -0,0 +1,14 @@
+﻿using BracketPairColorizer.Core.Settings;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace BracketPairColorizer.Core.XLangSupport
+{
+    internal static class FakeContentTypeDefinition
+    {
+        [Export]
+        [Name(ContentTypes.XLang)]
+        [BaseDefinition(ContentTypes.Code)]
+        internal static ContentTypeDefinition FakeXLang { get; set; }
+    }
+}
