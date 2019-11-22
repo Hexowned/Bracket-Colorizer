c24f9948cd00cb3cae9ebf63d083fb09d881bbfe
Nexowned <49051663+Nexowned@users.noreply.github.com>
2019-11-22 00:26:31 -0800

    Started on constraints for "Presentation Mode" Added the base state class to help with obfuscating and declaring if a user is in presentation mode or not and allow a toggle on / off switch for the plugin if it may affect the performance of any way. Started on empty files, need to wrap my head around the idea (time to read docs!)

---
+++ BracketPairColorizer.Core/Text/PresentationModeFactory.cs
@@ -0,0 +1,27 @@
+﻿using BracketPairColorizer.Core.Contracts;
+using BracketPairColorizer.Core.Settings;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace BracketPairColorizer.Core.Text
+{
+    [Export(typeof(IWpfTextViewCreationListener))]
+    [ContentType("any")]
+    [TextViewRole(PredefinedTextViewRoles.Interactive)]
+    public class PresentationModeFactory : IWpfTextViewCreationListener
+    {
+        [Import]
+        internal IVsfSettings Settings { get; set; }
+
+        [Import]
+        internal IPresentationModeState State { get; set; }
+
+        public void TextViewCreated(IWpfTextView textView)
+        {
+            textView.Properties.GetOrCreateSingletonProperty(
+              () => new PresentationMode(textView, State, Settings)
+            );
+        }
+    }
+}
---
+++ BracketPairColorizer.Core/Text/PresentationModeState.cs
@@ -0,0 +1,57 @@
+﻿using BracketPairColorizer.Core.Contracts;
+using BracketPairColorizer.Core.Settings;
+using System;
+using System.ComponentModel.Composition;
+
+namespace BracketPairColorizer.Core.Text
+{
+    [Export(typeof(IPresentationModeState))]
+    public class PresentationModeState : IPresentationModeState
+    {
+        public bool PresentationModeTurnedOn { get; private set; }
+        private PresentationModeFontChanger fontChanger;
+        private IVsfSettings settings;
+        private IVsfTelemetry telemetry;
+
+        public event EventHandler PresentationModeChanged;
+
+        [ImportingConstructor]
+        public IPresentationModeState(IVsfSettings settingsManager, IVsfTelemetry telemetry)
+        {
+            this.fontChanger = new PresentationModeFontChanger(this);
+            this.settings = settingsManager;
+            this.telemetry = telemetry;
+        }
+
+        public int GetPresentationModeZoomLevel()
+        {
+            return PresentationModeTurnedOn
+                ? this.settings.PresentationModeEnabledZoom
+                : this.settings.PresentationModeDefaultZoom;
+        }
+
+        public void TogglePresentationMode()
+        {
+            PresentationModeTurnedOn = !PresentationModeTurnedOn;
+            PresentationModeChanged?.Invoke(this, EventArgs.Empty);
+            if (PresentationModeTurnedOn)
+            {
+                this.fontChanger.TurnOn();
+                this.telemetry.WriteEvent("Presentation Mode");
+            } else
+            {
+                this.fontChanger.TurnOff();
+            }
+        }
+
+        public void TurnOff(bool notifyChanges)
+        {
+            this.fontChanger.TurnOff(notifyChanges);
+        }
+
+        public T GetService<T>()
+        {
+            return (T)ServiceProvider.GlobalProvider.GetService(typeof(T));
+        }
+    }
+}
