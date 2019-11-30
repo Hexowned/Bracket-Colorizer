e0eea66218f02b13cdd7805fb940171df1b3bcae
Nexowned <49051663+Nexowned@users.noreply.github.com>
2019-11-29 22:28:04 -0800

    Added configuration files for each of the projects Added commands for the extension users to utilize while in use of the extension ( which will run unless it is disabled and or some of its settings are disabled )

---
+++ BracketPairColorizer/Commands/ObfuscateTextCommand.cs
@@ -0,0 +1,36 @@
+﻿using BracketPairColorizer.Core.Compatibility;
+using BracketPairColorizer.Core.Contracts;
+using BracketPairColorizer.Core.Settings;
+using BracketPairColorizer.Core.Text;
+using Microsoft.VisualStudio.Shell;
+using System;
+
+namespace BracketPairColorizer.Commands
+{
+    public class ObfuscateTextCommand : VisualStudioCommand
+    {
+        private IBpcTelemetry telemetry;
+
+        public ObfuscateTextCommand(BpcPackage package, OleMenuCommandService omcs)
+            : base(package, omcs)
+        {
+            Initialize(new Guid(Guids.guidBpcViewCmdSet), PkgCmdIdList.cmdidObfuscateText);
+            var model = new SComponentModel();
+            this.telemetry = model.GetService<IBpcTelemetry>();
+        }
+
+        protected override void OnBeforeQueryStatus(object sender, EventArgs e)
+        {
+            base.OnBeforeQueryStatus(sender, e);
+            Command.Checked = TextObfuscationState.Enabled;
+            Command.Enabled = true;
+        }
+
+        protected override void OnInvoke(object sender, EventArgs e)
+        {
+            base.OnInvoke(sender, e);
+            TextObfuscationState.Invert();
+            this.telemetry?.WriteEvent("Obfuscate Text");
+        }
+    }
+}
---
+++ BracketPairColorizer/Commands/PresentationModeCommand.cs
@@ -0,0 +1,35 @@
+﻿using BracketPairColorizer.Core.Compatibility;
+using BracketPairColorizer.Core.Contracts;
+using BracketPairColorizer.Core.Settings;
+using Microsoft.VisualStudio.Shell;
+using System;
+
+namespace BracketPairColorizer.Commands
+{
+    public class PresentationModeCommand : VisualStudioCommand
+    {
+        private IBpcSettings settings = SettingsContext.GetSettings();
+        private IPresentationModeState state;
+
+        public PresentationModeCommand(BpcPackage package, OleMenuCommandService omcs)
+            : base(package, omcs)
+        {
+            var model = new SComponentModel();
+            this.state = model.GetService<IPresentationModeState>();
+            Initialize(new Guid(Guids.guidBpcViewCmdSet), PkgCmdIdList.cmdidPresentationMode);
+        }
+
+        protected override void OnBeforeQueryStatus(object sender, EventArgs e)
+        {
+            base.OnBeforeQueryStatus(sender, e);
+            Command.Checked = this.state.PresentationModeTurnedOn;
+            Command.Enabled = this.settings.PresentationModeEnabled;
+        }
+
+        protected override void OnInvoke(object sender, EventArgs e)
+        {
+            base.OnInvoke(sender, e);
+            this.state.TogglePresentationMode();
+        }
+    }
+}
